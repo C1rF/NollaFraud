e170dbbaa569cac37135ada7e611ff2c4f0b0bb9
Mingxuan Fan <1417388068@qq.com>
2022-11-16 13:33:55 +0800

    modified train.py

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 import argparse
 from sklearn.model_selection import train_test_split
 from utils import *
+from layers import *
 
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
 
 
 """
@@ -16,11 +16,11 @@ parser = argparse.ArgumentParser()
 
 # dataset and model dependent args
 parser.add_argument('--data', type=str, default='amazon', help='The dataset name. [Amazon_demo, Yelp_demo, amazon,yelp]')
-parser.add_argument('--batch_size', type=int, default=100, help='Batch size 1024 for yelp, 256 for amazon.')
+parser.add_argument('--batch_size', type=int, default=32, help='Batch size 1024 for yelp, 256 for amazon.')
 parser.add_argument('--lr', type=float, default=0.1, help='Initial learning rate. [0.1 for amazon and 0.001 for yelp]')
 parser.add_argument('--lambda_1', type=float, default=1e-4, help='Weight decay (L2 loss weight).')
 parser.add_argument('--embed_dim', type=int, default=64, help='Node embedding size at the first layer.')
-parser.add_argument('--num_epochs', type=int, default=61, help='Number of epochs.')
+parser.add_argument('--num_epochs', type=int, default=21, help='Number of epochs.')
 parser.add_argument('--test_epochs', type=int, default=10, help='Epoch interval to run test set.')
 parser.add_argument('--seed', type=int, default=123, help='Random seed.')
 parser.add_argument('--no_cuda', action='store_true', default=False, help='Disables CUDA training.')
@@ -31,6 +31,9 @@ print(f'run on {args.data}')
 
 # load topology, feature, and label
 homo, relation1, relation2, relation3, feat_data, labels = load_data(args.data)
+
+feat_data = normalize(feat_data) 
+adj_lists = [relation1, relation2, relation3]
 
 np.random.seed(args.seed)
 random.seed(args.seed)
@@ -49,23 +52,20 @@ elif args.data == 'amazon':
 else:
 	exit("Dataset not supported")
 
+num_1 = len(np.where(y_train == 1)[0])
+num_2 = len(np.where(y_train == 0)[0])
+p0 = (num_1 / (num_1 + num_2))
+p1 = 1 - p0
+prior = np.array([p1, p0])
+
 # Construct tensorflow dataset
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 train_dataset = train_dataset.batch(args.batch_size)
 test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 test_dataset = test_dataset.batch(args.batch_size)
 
-# TODO: Replace the dummy model with the actual gnn model
-dummy_model = keras.Sequential([
-    layers.Dense(1, input_shape=[1]),
-    layers.Dense(1, activation='relu'),
-    layers.Dense(1, activation='sigmoid')
-])
-model = dummy_model
-
-# TODO: Replace the dummy loss object with the actual loss function
-dummy_loss = keras.losses.BinaryCrossentropy(from_logits=True)
-loss = dummy_loss
+model = NollaFraud(feat_data, adj_lists, prior)
+loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 optimizer = keras.optimizers.Adam(learning_rate=args.lr)
 
@@ -78,48 +78,53 @@ test_accuracy_results = []
 for epoch in range(args.num_epochs):
 
 	train_epoch_loss_avg = keras.metrics.Mean()
-	train_epoch_accuracy = keras.metrics.BinaryAccuracy()
+	train_epoch_accuracy = keras.metrics.SparseCategoricalAccuracy()
 
 	for _, (batch_nodes, batch_label) in enumerate(train_dataset):
 		# Optimize the model
 		with tf.GradientTape() as tape:
 			# Obtain loss and grads
-			loss_value = loss(y_true=batch_label, y_pred=model(batch_nodes))
-			grads = tape.gradient(loss_value, model.trainable_variables)
+			logits = model(batch_nodes, training=True)
+			preds = tf.math.argmax(tf.math.sigmoid(logits), axis=1)
+			print_with_color("PREDICTION:")
+			print_with_color(tf.cast(preds, tf.int32))
+			print_with_color("LABELS:")
+			print_with_color(tf.cast(batch_label, tf.int32))
+			loss_value = loss_fn(batch_label, logits)
+			grads = tape.gradient(loss_value, model.trainable_weights)
 			# Optimize
-			optimizer.apply_gradients(zip(grads, model.variables))
+			optimizer.apply_gradients(zip(grads, model.trainable_weights))
 			# Track status
 			train_epoch_loss_avg.update_state(loss_value)  # Add current batch loss
-			train_epoch_accuracy.update_state(batch_label, model(batch_nodes))
+			train_epoch_accuracy.update_state(batch_label, logits)
 
-	# End epoch
-	train_loss_results.append(train_epoch_loss_avg.result())
-	train_accuracy_results.append(train_epoch_accuracy.result())
+			# # End epoch
+			# train_loss_results.append(train_epoch_loss_avg.result())
+			# train_accuracy_results.append(train_epoch_accuracy.result())
 
-	if epoch % 10 == 0:
-		print("Train =====> Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                train_epoch_loss_avg.result(),
-                                                                train_epoch_accuracy.result()))
+			# if epoch % 10 == 0:
+			print("Train =====> Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+																		train_epoch_loss_avg.result(),
+																		train_epoch_accuracy.result()))
+			train_epoch_loss_avg.reset_state()
+			train_epoch_accuracy.reset_state()
 # Test
-for epoch in range(args.test_epochs):
-	test_epoch_loss_avg = keras.metrics.Mean()
-	test_epoch_accuracy = keras.metrics.BinaryAccuracy()
-
-	for _, (batch_nodes, batch_label) in enumerate(test_dataset):
-		# Optimize the model
-		with tf.GradientTape() as tape:
-			# Obtain loss
-			loss_value = loss(y_true=batch_label, y_pred=model(batch_nodes))
-			# Track status
-			test_epoch_loss_avg.update_state(loss_value)  # Add current batch loss
-			test_epoch_accuracy.update_state(batch_label, model(batch_nodes))
-
-	# End epoch
-	test_loss_results.append(test_epoch_loss_avg.result())
-	test_accuracy_results.append(test_epoch_accuracy.result())
-
-	if epoch == args.test_epochs - 1:
-		print("Test =====> Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+print("running test")
+test_epoch_loss_avg = keras.metrics.Mean()
+test_epoch_accuracy = keras.metrics.SparseCategoricalCrossentropy()
+for _, (batch_nodes, batch_label) in enumerate(test_dataset):
+	# Optimize the model
+	with tf.GradientTape() as tape:
+		# Obtain loss
+		loss_value = loss_fn(y_true=batch_label, y_pred=model(batch_nodes))
+		# Track status
+		test_epoch_loss_avg.update_state(loss_value)  # Add current batch loss
+		test_epoch_accuracy.update_state(batch_label, model(batch_nodes))
+# End epoch
+test_loss_results.append(test_epoch_loss_avg.result())
+test_accuracy_results.append(test_epoch_accuracy.result())
+# if epoch == args.test_epochs - 1:
+print("Test =====> Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
                                                                 test_epoch_loss_avg.result(),
                                                                 test_epoch_accuracy.result()))
 
